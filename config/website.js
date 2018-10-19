b1b0b182c900a7204fbf7782bd7e9a865c3c3ebc
ray lot <rlotmar@hotmail.com>
2018-10-19 14:51:33 +0200

    first commit

--- config/website.js
+++ config/website.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  siteTitle: 'Emma', // Navigation and Site Title
-  siteTitleAlt: 'Emma - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-emma.netlify.com', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
+  siteTitle: 'Moosartig', // Navigation and Site Title
+  siteTitleAlt: 'Moosart Garten und pflanzen Kunst ', // Alternative Site title for SEO
+  siteUrl: 'https://moosartig.ch', // Domain of your site. No trailing slash!
+  siteLanguage: 'de', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteDescription: 'Minimalistic bright portfolio with full-width grid and large images',
   author: 'LekoArts', // Author for schemaORGJSONLD
